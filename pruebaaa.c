ff7ce26f9777ef3fa5bc29639f957aef096ac9fe
Crhystel <crhystel.velasco@udla.edu.ec>
2023-05-03 15:41:59 -0500

    prueba incial

--- pruebaaa.c
+++ pruebaaa.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
-// este programa indica cuantos numeros se leyeron para que la suma de los negativos de mas de 1200
-
+// esta es la prueba inicial
 int main (){
-    int n, sumne=0, i=0;
+    int x,y, n, sum;
     while (n!=0){
-        printf("Ingrese un numero entero para realizar la suma\n");
+        printf("Ingrese un numero entero para realizar la suma");
         scanf("%d", &n);
-        i+=1;
-        if (n<0 && abs(sumne) <= 1200){
-        
-            sumne+=n;
-            printf("Se leyeron %d numero/s negativos con valor absoluto a 1200 ", i);
+        if (n>0 && sum<1200){
+            sum=n+n;
         }
         
     }
+   
+    
+
+
+    
+    
     return 0;
 }
