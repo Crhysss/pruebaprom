10a270ca3bc85a892de3a6d46c28f35e09143542
Crhystel <crhystel.velasco@udla.edu.ec>
2023-05-03 16:11:33 -0500

    inicial inicial

---
+++ prueba2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// esta es la prueba inicial
+int main (){
+    int x,y, n, sum;
+    while (n!=0){
+        printf("Ingrese un numero entero para realizar la suma");
+        scanf("%d", &n);
+        if (n>0 && sum<1200){
+            sum=n+n;
+        }
+        
+    }
